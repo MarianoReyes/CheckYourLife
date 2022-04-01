717a9ba6d51a9686a4977b6503b3321bdfad05ad
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2022-04-01 11:54:41 -0600

    Cambios en Perfil y Login

--- CheckYourLife/screens/Login.js
+++ CheckYourLife/screens/Login.js
@@ -124,7 +124,7 @@ const styles = StyleSheet.create({
     marginTop: 40,
   },
   button: {
-    backgroundColor: '#0782F9',
+    backgroundColor: '#14279B',
     width: '100%',
     padding: 15,
     borderRadius: 10,
@@ -142,7 +142,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   buttonOutlineText: {
-    color: '#0782F9',
+    color: '#14279B',
     fontWeight: '700',
     fontSize: 16,
   },
--- CheckYourLife/screens/Perfil.js
+++ CheckYourLife/screens/Perfil.js
@@ -2,9 +2,10 @@
 //import { firebase } from '@react-native-firebase/auth';
 //import { useNavigation } from '@react-navigation/core'
 import React from 'react'
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Alert, StyleSheet, Text, TouchableOpacity, View, TextStyle,Image} from 'react-native'
 import { auth } from '../firebase';
 import { signOut } from "firebase/auth";
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 const Perfil = () => {
@@ -14,14 +15,29 @@ const Perfil = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity
-        onPress={handleSignOut}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity>
+    <View>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{padding:10,width:'100%',backgroundColor:'#14279B',height:150}}> 
+        </View>
+        <View style={{alignItems:'center'}}>
+          <Image source={require('../assets/PerfilTemp.png')} style={{width:140,height:140,borderRadius:100,marginTop:-70}}/>
+        </View>
+        <View style={styles.container}>
+          <Text>Email: {auth.currentUser?.email}</Text>
+        </View>
+        <View style={styles.button1}>
+          <Text>Product Designer</Text>
+        </View>
+        <View style={styles.button1}>
+          <Text>Guatemala,Guatemala</Text>
+        </View>
+        <View style={styles.button1}>
+          <Text>Share</Text>
+        </View>
+        <TouchableOpacity onPress={handleSignOut} style={styles.button}>
+          <Text style={styles.buttonText}>Sign out</Text>
+          </TouchableOpacity>
+      </ScrollView>
     </View>
   )
 }
@@ -35,16 +51,36 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
    button: {
-    backgroundColor: '#0782F9',
-    width: '60%',
-    padding: 15,
-    borderRadius: 10,
-    alignItems: 'center',
-    marginTop: 40,
+    alignSelf:'center',
+    flexDirection:'row',
+    justifyContent:'center',
+    backgroundColor: '#14279B',
+    width:'90%',
+    padding:20,
+    paddingBottom:22,
+    borderRadius:10,
+    shadowOpacity:80,
+    elevation:15,
+    marginTop:20,
+    marginBottom:20
   },
   buttonText: {
     color: 'white',
     fontWeight: '700',
     fontSize: 16,
   },
+  button1: {
+    alignSelf:'center',
+    flexDirection:'row',
+    justifyContent:'center',
+    backgroundColor: '#fff',
+    width:'90%',
+    padding:20,
+    paddingBottom:22,
+    borderRadius:10,
+    shadowOpacity:80,
+    elevation:15,
+    marginTop:20,
+    marginBottom:10
+  }
 })
