0fb00b0ed200e8c5434d0b3d93d99cb3dcd13c1d
Nate20029 <70552363+Nate20029@users.noreply.github.com>
2022-04-30 03:35:10 -0600

    Diseño de pantalla CHAT

--- CheckYourLife/screens/Comunidad.js
+++ CheckYourLife/screens/Comunidad.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, SectionList, StatusBar, TouchableOpacity} from "react-native";
-import { Button } from "react-native-elements";
 import Icon1 from "react-native-vector-icons/MaterialIcons";
 
 
@@ -19,13 +18,14 @@ const comunidad = ({navigation}) => {
   ];
 
   const onPress = ()=> navigation.navigate('CHAT')
-  
+
   const Item = ({ title }) => (
     <View style={styles.item} >
       <TouchableOpacity 
         onPress={onPress}
       >
-        <Icon1 name="person" style={styles.icon1}>  </Icon1>
+        <Icon1 name="person" style={styles.icon1} >  </Icon1>
+
         <Text style={styles.title}  >{title}  </Text>
 
       </TouchableOpacity>
@@ -57,12 +57,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: StatusBar.currentHeight,
-    marginHorizontal: 16
+    marginLeft: 15,
+    marginRight:15,
+    flexDirection:'row',
+    alignContent:'center'
   },
   item: {
     backgroundColor: "#E6E6E6",
-    padding: 10,
-    marginVertical: 8
+    marginVertical: 8,
   },
 
   header: {
@@ -73,21 +75,14 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 34,
-    textAlign: 'center',
+    textAlign:'center',
 
   },
   icon1: {
     color: "rgba(128,128,128,1)",
     fontSize: 60,
-    alignContent: "left",
 
-  },
-  boton: {
-    color:'black',
-    fontSize: 60,
-    alignContent: "left",
-
-  },
+  }
 
 });
 
