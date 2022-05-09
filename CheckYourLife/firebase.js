048eb770e081c68c8e9407f01bbdf0cb8af4b8f1
JuanCarlosBajan <baj20109@uvg.edu.gt>
2022-05-08 22:37:13 -0600

    data base config

--- CheckYourLife/firebase.js
+++ CheckYourLife/firebase.js
@@ -1,8 +1,9 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, onAuthStateChanged,updateProfile } from 'firebase/auth';
-import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
-import { useEffect, useState } from "react";
+import { initializeApp } from 'firebase/app'
+import { getAuth, onAuthStateChanged, updateProfile } from 'firebase/auth'
+import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,49 +11,50 @@ import { useEffect, useState } from "react";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA8Vc2Cw2TFHfxooq-GlayNk-4kxPE8Qic",
-  authDomain: "check-your-life-8d160.firebaseapp.com",
-  projectId: "check-your-life-8d160",
-  storageBucket: "check-your-life-8d160.appspot.com",
-  messagingSenderId: "829574262711",
-  appId: "1:829574262711:web:3d178b998e283f66f84232",
-  measurementId: "G-24EYTZK046"
-};
+  apiKey: 'AIzaSyA8Vc2Cw2TFHfxooq-GlayNk-4kxPE8Qic',
+  authDomain: 'check-your-life-8d160.firebaseapp.com',
+  projectId: 'check-your-life-8d160',
+  storageBucket: 'check-your-life-8d160.appspot.com',
+  messagingSenderId: '829574262711',
+  appId: '1:829574262711:web:3d178b998e283f66f84232',
+  measurementId: 'G-24EYTZK046',
+}
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-onAuthStateChanged(auth, user => { console.log(user); });
-const storage = getStorage();
+const firebaseApp = initializeApp(firebaseConfig)
+const auth = getAuth(firebaseApp)
+onAuthStateChanged(auth, (user) => {
+  console.log(user)
+})
+const storage = getStorage()
+const db = getFirestore()
 
-
-
-export {auth};
-
+export { auth }
+export { db }
 
 // Custom Hook
 export function useAuth() {
-  const [currentUser, setCurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState()
 
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth, user => setCurrentUser(user));
-    return unsub;
+    const unsub = onAuthStateChanged(auth, (user) => setCurrentUser(user))
+    return unsub
   }, [])
 
-  return currentUser;
+  return currentUser
 }
 
 // Storage
 export async function upload(file, currentUser, setLoading) {
-  const fileRef = ref(storage, currentUser.uid + '.png');
+  const fileRef = ref(storage, currentUser.uid + '.png')
 
-  setLoading(true);
-  
-  const snapshot = await uploadBytes(fileRef, file);
-  const photoURL = await getDownloadURL(fileRef);
+  setLoading(true)
 
-  updateProfile(currentUser, {photoURL});
-  
-  setLoading(false);
-  alert("Uploaded file!");
+  const snapshot = await uploadBytes(fileRef, file)
+  const photoURL = await getDownloadURL(fileRef)
+
+  updateProfile(currentUser, { photoURL })
+
+  setLoading(false)
+  alert('Uploaded file!')
 }
